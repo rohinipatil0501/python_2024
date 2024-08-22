54585422b8e63332fbdbc383b60f1b60175772d6
rohinipatil0501 <rohinipatil05012005@gmail.com>
2024-08-22 16:05:14 +0530

    program done

--- p19.py
+++ p19.py
@@ -1,6 +1,6 @@
 #Program to print the Right angled Triangle of N lines
 number_of_lines=int(input('enter number of lines'))
-for i in range(1,number_of_lines+1):
+for i in range(0,number_of_lines):
     for j in range(i):
         print('*',end=' ')
     print()
